16f481ff1b6166ff71ecfe497729b02845e748b6
federico-palma <fedepal9@gmail.com>
2022-01-30 10:34:30 +0100

    Added basic operators

---
+++ script.js
@@ -0,0 +1,36 @@
+// Math operators
+function add(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+    
+        case '-':
+            return substract(a, b);
+            break;
+    
+        case '*':
+            return multiply(a, b);
+            break;
+    
+        case '/':
+            return divide(a, b);
+            break;
+    
+        default:
+            break;
+    }
+}
